abaa6ad835fb6cfc847a45cc55e8d697a58048f6
Caroline <carolnte@gmail.com>
2021-03-09 14:33:55 -0300

    Nodemon instalado

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.listen(port, (require, response)=>{
+app.listen(port, ()=>{
   console.log("Server is running!")
 });
