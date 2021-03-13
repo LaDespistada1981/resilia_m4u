f2007ce3966397f8dc617456797ccb580d955be2
Caroline <carolnte@gmail.com>
2021-03-13 19:56:13 -0300

    app.js modificado

---
+++ backend/src/controller/services-controller.js
@@ -0,0 +1,10 @@
+//Recarga
+
+app.post('/services/recharge', (req, resp) => {
+    console.log("Vai uma recarga aí, filha?")
+  })
+  
+  //Compra 
+  app.post('/services/buy', (req, resp) => {
+    console.log("Vai um chip aí, filha?")
+  })
