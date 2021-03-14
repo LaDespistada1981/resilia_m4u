53671b7ee4780859893d51521e17378c55b021b9
Caroline <carolnte@gmail.com>
2021-03-14 19:01:32 -0300

    Cors adicionado

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
 
 app.listen(port, ()=>{
   console.log("Server is running!")
