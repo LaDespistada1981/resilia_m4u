479e7ef49d93669690c9f37198274a14cb4a1616
Caroline <carolnte@gmail.com>
2021-03-17 01:05:11 -0300

    Roteamento de login e cadastro parcialmente feitos (falta validar com banco)

--- backend/src/DAO/users-DAO.js
+++ backend/src/DAO/users-DAO.js
@@ -15,4 +15,24 @@ module.exports = class usersDAO{
     }
 
 
+    getUserByEmailInDB(email){
+        return (new Promise((resolve, reject) => {
+            this.bd.all(`SELECT * FROM user WHERE email = ?`, [email], (err, rows) =>{
+                if(err) reject(err)
+                else resolve(rows)  
+            })
+        }
+                
+        ))
+    }
+
+    createUser(name, email, cpf, phone, user_id){
+        return ( new Promise((resolve, reject) =>{
+            this.bd.run(`INSERT INTO USERS (NAME, EMAIL, CPF, PHONE, USER_ID) VALUES (?,?,?,?,?)`, [name, email, cpf, phone, user_id], (error) =>{
+                if (error) reject (error +". Please try again.") 
+                else resolve ("Register completed successfully!")})
+            })
+        )
+    }
+
 }
--- backend/src/controller/users-controller.js
+++ backend/src/controller/users-controller.js
@@ -1,42 +1,28 @@
 const usersDAO = require('../DAO/users-DAO')
-const bd = require('../infra/sqlite-db')
+
 
 module.exports = (app, bd) => {
 
     const uDAO = new usersDAO(bd)
 
- 
-  //Cadastro de usuário
-
-  app.get('/user/register', async (req, resp) => 
-  {
-          try{
-              const answer = await uDAO.showUsers();
-              resp.status(200).send(answer);
-          }
-          catch(error){
-              resp.send(error);
-          }
-  })
-
-  //Login
-app.post('/user/login', (req, resp) =>{
-  console.log('Oi, eu sou o login')
-})
+    //Login
+  app.post('/user/login', async  (req, resp) => {
+          const user = await uDAO.getUserByEmailInDB(req.body.email)
+          if(req.body.password === user.password){
+              resp.send(`You're in`)} 
+          else {
+              resp.send(`Wrong password. Please try again.`)}  
+      })
   
+
+  //Register
   app.get('/user/register/:id', async (req, resp) => 
   {
-          try{
-              const retorna = await tarDAO.listaTarefas();
-              resp.status(200).send(retorna);
-          }
-          catch(error){
-              resp.send(error);
-          }
+
   })
   
   app.post('/user/register', (req, resp)=>{
-    console.log("Oi, eu sou o post do cadastro")
+    resp.send(req.body.name, req.body.cpf, req.body.phone)
   })
   
   app.put('/user/register/:id', (req, resp)=>{
