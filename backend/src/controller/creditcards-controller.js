476f21fbb598f1c8d636504c40ff72d663da0f5a
justo-marcella <justoamarcella@gmail.com>
2021-03-23 01:28:17 -0300

    Registro de cartoes atualizado

--- backend/src/controller/creditcards-controller.js
+++ backend/src/controller/creditcards-controller.js
@@ -7,27 +7,37 @@ module.exports = (app,bd) =>{
 
     const cDAO = new creditCardsDAO(bd) 
 
+   //criptografia cartão
+// app.post('/creditcards/register', async (req, resp)=>{
+//   try{
+//    const hash = generateHash(req.body.block_2)
+//    const addCreditCard = await cDAO.collectCreditCard([req.body.type, req.body.brand, req.body.block_1, hash, req.body.block_3, req.body.holder, req.body.expirationdate, req.body.id_user]);
+//    resp.send("Credit card successfully saved")
+//   }
+//   catch(error){
+//     resp.send(error);
+//   }
+
+// })
+
 app.post('/creditcards/register', async (req, resp)=>{
   try{
-   const hash = generateHash(req.body.block_2)
-   const addCreditCard = await cDAO.collectCreditCard([req.body.type, req.body.brand, req.body.block_1, hash, req.body.block_3, req.body.holder, req.body.expirationdate, req.body.id_user]);
-   resp.send("Credit card successfully saved")
-  }
-  catch(error){
+    const addCreditCard = cDAO.collectCreditCard([req.body.type, req.body.brand, req.body.number, req.body.holder, req.body.expirationdate, req.body.id_user])
+    resp.send("Credit card successfully saved")
+  }catch(error){
     resp.send(error);
   }
-
 })
 
   app.get("/creditcards",async (req, resp) => {
-                try{
-                    const cardsData = await cDAO.creditCardsDatabase();
-                    resp.status(200).send(cardsData);
-                }
-                catch(error){
-                    resp.send(error);
-                }
-        })
+      try{
+          const cardsData = await cDAO.creditCardsDatabase();
+          resp.status(200).send(cardsData);
+        }
+      catch(error){
+          resp.send(error);
+        }
+  });
   
   app.get('/creditcards/:ID_USER', async (req, resp)=>{
       try{
