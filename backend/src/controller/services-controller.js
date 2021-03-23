6b9cabff1320fec9442b4698c5b4a9d979b31850
justo-marcella <justoamarcella@gmail.com>
2021-03-22 22:55:24 -0300

    Registra compra

--- backend/src/controller/services-controller.js
+++ backend/src/controller/services-controller.js
@@ -11,7 +11,7 @@ app.post("/services/recharge", (req, resp) => {
 });
 
 //Compra
-app.post("/services/buy", async (req, resp) => {
+app.post("/services/buy-chip", async (req, resp) => {
   try{
     const addChip = await sDAO.generateChip([req.body.msisdn, req.body.company, req.body.id_user]);
     resp.send(`Chip successfully generated!`)
@@ -20,4 +20,38 @@ app.post("/services/buy", async (req, resp) => {
      resp.send(error);
    }
 })
+
+//Faz registro de Venda
+app.post("/services/sale", async (req, resp)=>{
+  try{
+    const addSale = await sDAO.generateSale([req.body.date, req.body.product, req.body.msisdn, req.body.amount, req.body.id_user, req.body.id_card]);
+    resp.send(`Successful purchase.`)
+  }catch(error){
+    resp.send(error);
+  }
+});
+
+//Gera registros de venda
+app.get('/services/sale', async (req, resp)=>{
+  try{
+    const allSales = await sDAO.showSales();
+    resp.send(allSales)
+
+  }catch(error){
+    resp.send(error);
+  }});
+
+//Histórico de Compras Usuário
+app.get('/services/:USER_ID', async (req, resp)=>{
+  try{
+    const userEmail = await uDAO.getUserByEmail([req.params.ID_USER])
+  
+    resp.send(userEmail)
+  }
+  catch(error){
+    resp.send(error)
+  }
+})
+
+
 }
