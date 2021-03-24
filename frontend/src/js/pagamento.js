a1dc5b71631d3e3a12f7cd641b0a2b211c7c9dd4
Roberta Santos <robertasantosrj@gmail.com>
2021-03-24 15:15:51 -0300

    Atualizando front/js/cadastro-login-pagamento-recarga-recsenha

--- frontend/src/js/pagamento.js
+++ frontend/src/js/pagamento.js
@@ -1,3 +1,7 @@
+let divPg = document.getElementById('payments');
+let divForm = document.getElementById('contentForme');
+let divNome = document.getElementById('contentNome');
+let divcheck = document.getElementById('seCad');
 let numCartao = document.getElementById('numCartao');
 let nome = document.getElementById('nome');
 let cvv = document.getElementById('cvv');
@@ -11,35 +15,45 @@ let numCard = numCartao.value;
 let name = nome.value;
 let expiration = validade.value;
 
+botaoPag.addEventListener("click", ()=>{
+    divPg.innerHTML = "<br><center>Pagamento efetuado com sucesso!</center>"
+    divForm.innerHTML = '<br>'
+    divNome.innerHTML = '<br>'
+    cadastra.innerHTML = '<br>'
+    divcheck.innerHTML = '<br>'
+    botaoPag.innerHTML = "<center><a href='../../index.html'>Home</a></center>"
+     
+
+});
+
 //Tratamento erros
 
-botaoPag.addEventListener("click", ()=>{
-    if (numCard == ""){
-        numCartao.style.backgroundColor = "#E5E5E5"
-        numCartao.placeholder = "Insira um número de cartão válido"
-    }
+// botaoPag.addEventListener("click", ()=>{
+//     if (numCard == ""){
+//         numCartao.style.backgroundColor = "#E5E5E5"
+//         numCartao.placeholder = "Insira um número de cartão válido"
+//     }
 
-    if (nome.value == ""){
-        nome.style.backgroundColor = "#E5E5E5"
-        nome.placeholder = "Insira um nome válido"
-    }
+//     if (nome.value == ""){
+//         nome.style.backgroundColor = "#E5E5E5"
+//         nome.placeholder = "Insira um nome válido"
+//     }
 
-    if (cvv.value == ""){
-        cvv.style.backgroundColor = "#E5E5E5"
-        cvv.placeholder = "Insira um CVV"
-    }
+//     if (cvv.value == ""){
+//         cvv.style.backgroundColor = "#E5E5E5"
+//         cvv.placeholder = "Insira um CVV"
+//     }
 
-    if (validade.value == ""){
-        validade.style.backgroundColor = "#E5E5E5"
-        validade.placeholder = "Insira uma data de expiração"
-    }
-})
+//     if (validade.value == ""){
+//         validade.style.backgroundColor = "#E5E5E5"
+//         validade.placeholder = "Insira uma data de expiração"
+//     }
+// })
 
 
-//Reaizar cadastro
+// //Reaizar cadastro
 
-cadastra.addEventListener("change", ()=>{
-    console.log("Cadastra")
-})
-
+// cadastra.addEventListener("change", ()=>{
+//     console.log("Cadastra")
+// })
 
--- frontend/src/js/recuperacaoSenha.js
+++ frontend/src/js/recuperacaoSenha.js
@@ -5,24 +5,32 @@ let submit = document.getElementById('submit')
 let emailValue = email.value;
 let password = novaSenha.value;
 let confirmPassword = confirmaNovaSenha.value;
+let modal = document.querySelector('.modal-body')
 
 submit.addEventListener("click", ()=>{
-    if (emailValue == ""){
+  /*   if (emailValue == ""){
         email.placeholder = "Por favor, insira um email"
         email.style.backgroundColor = "#E5E5E5"
-    }
+        if (password == ""){
+                novaSenha.placeholder = "Por favor, insira a senha"
+                novaSenha.style.backgroundColor = "#E5E5E5"
+                if (confirmPassword == ''){
+                    confirmaNovaSenha.placeholder = "Por favor, confirme a senha"
+                    confirmaNovaSenha.style.backgroundColor = "#E5E5E5"
+                }    }  }
 
-    if (password == ""){
-        novaSenha.placeholder = "Por favor, insira a senha"
-        novaSenha.style.backgroundColor = "#E5E5E5"
-    }
+    else{  */
+        modal.innerHTML = `<h3>Confirmação de senha enviada para o seu email!</h3>` 
 
-    if (confirmPassword == ''){
-        confirmaNovaSenha.placeholder = "Por favor, confirme a senha"
-        confirmaNovaSenha.style.backgroundColor = "#E5E5E5"
-    }
+    /*  }  */
+
+
 
 })
+
+
+
+
 
 
 
